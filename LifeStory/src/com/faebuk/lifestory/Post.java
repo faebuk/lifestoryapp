919a8f0c786e65fe2567ec3c90c3a4a0946f27a7
faebuk <faebuk@gmail.com>
2014-03-13 10:01:20 +0100

    Create Post.java

---
+++ LifeStory/src/com/faebuk/lifestory/Post.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+
+public class Post {
+	
+	int postid;
+	String datum;
+	String title;
+	String description;
+	Media media;
+	User user;
+	
+	public Post(){
+		
+	}
+	
+	protected String getTitle(){
+		return null;
+	}
+	
+	protected String getDescription(){
+		return null;
+	}
+	
+	protected Media getMedia(){
+		return null;
+	}
+	
+	protected User getUser(){
+		return null;
+	}
+	
+	protected Date getDate(){
+		return null;
+	}
+
+}
